fb2714aa55dc1153b159f48096debdd5b977a010
Hongjuseong <hjs21847@naver.com>
2021-04-12 18:07:56 +0900

    11번 문제 풀이

--- section_01/11. 문자열에서 대문자 찾기.js
+++ section_01/11. 문자열에서 대문자 찾기.js
@@ -1,2 +1,10 @@
-// 작업 예정
-// 깃 오류나는지 테스트 중
+// 입력된 문자열에서 대문자가 몇 개 있는지 찾기
+
+function capital(str) {
+  let count = 0;
+  count = str.match(/[A-Z]/g).length;
+
+  return count;
+}
+
+console.log(capital('KoreaTimeGood'));
