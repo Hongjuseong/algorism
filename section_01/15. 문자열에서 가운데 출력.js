be40aa463e530c3a4f0adb86c8251560b4de59e8
Hongjuseong <hjs21847@naver.com>
2021-04-14 21:42:33 +0900

    15번 풀이

---
+++ section_01/15. 문자열에서 가운데 출력.js
@@ -0,0 +1,22 @@
+// 입력된 소문자 문자열에서 가운데 문자 출력 
+// 단어의 길이가 짝수면 가운데 2개 문자 출력
+// 단어의 길이가 홀수면 가운데 1개 문자 출력
+
+function middle(str) {
+  let odd;
+  let result;
+  const val = Math.floor(str.length / 2);
+
+  if (str.length % 2 === 1) odd = true;
+  else odd = false;
+
+  if (odd === true) result = str.substr(val, 1);
+  else result = str.substr(val - 1, 2);
+
+  return result;
+}
+
+console.log(middle('study'));
+console.log(middle('good'));
+console.log(middle('normal'));
+console.log(middle('something'));
