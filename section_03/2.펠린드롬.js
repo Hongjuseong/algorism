cf3cc0263b77fadda27884efbc4e5ce18e985b86
Hongjuseong <hjs21847@naver.com>
2021-04-22 18:25:44 +0900

    섹션 3 2번 풀이

--- section_03/2.펠린드롬.js
+++ section_03/2.펠린드롬.js
@@ -1,1 +1,17 @@
 // 팰린드롬이면 YES, 아니면 NO
+function solution(str) {
+  let result = 'NO';
+  const lower = str.toLowerCase();
+  
+  // a-z가 아닌(^) 녀석들 replace
+  const newStr = lower.replace(/[^a-z]/g, '');
+  const reverse = newStr.split('').reverse().join('');
+
+  if (newStr === reverse) {
+    result = 'YES';
+  }
+
+	return result;
+} 
+
+console.log(solution('found7, time: study; Yduts; emit, 7Dnuof'));
