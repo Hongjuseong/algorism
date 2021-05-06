b0605c79bfbdc48e027e4c9425b218e0b775731b
Hongjuseong <hjs21847@naver.com>
2021-05-06 21:24:38 +0900

    섹션 5 3~4번 풀이 4번은 틀림

--- section_05/4. 연속 부분 수열2.js
+++ section_05/4. 연속 부분 수열2.js
@@ -2,8 +2,6 @@ function concat(arr, num) {
   const aLen = arr.length;
   let result = 0;
   let sum = 0;
-  
-  // num은 부분합 값
 
   // 2개 포인터
   let point1 = 0;
@@ -11,24 +9,30 @@ function concat(arr, num) {
 
   while (point1 < aLen) {
 
-    if (point2 === aLen) {
-      break;
-    }
-
-    if (sum < num) {
+    if (sum < num && point2 !== aLen) {
       sum += arr[point2];
+
+      if (sum <= num) {
+        result++;
+      }
+
       point2++;      
+      
     } else {
       sum -= arr[point1];
-      point1++;
-    }
+      
+      if (sum <= num) {
+        result++;
+      }
 
-    if (sum === num) {
-      result++;
+      point1++;
     }
   }
 
   return result;
 }
 
-console.log(concat([1, 2, 1, 3, 1, 1, 1, 2], 6));
+console.log(concat([1, 3, 1, 2, 3], 5));
+
+
+
