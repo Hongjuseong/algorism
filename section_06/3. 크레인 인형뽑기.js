51a97d0c851c91c9cd922f8c6ecc253ade0bd56c
Hongjuseong <hjs21847@naver.com>
2021-05-18 20:43:55 +0900

    섹션 6 3번 풀이

---
+++ section_06/3. 크레인 인형뽑기.js
@@ -0,0 +1,36 @@
+function solution(board, moves) {
+  const stack = [];
+  let cnt = 0;
+
+  // 크랭크
+  for (let i = 0; i < moves.length; i++) {
+    moves[i]--;
+  }
+
+  // 인형뽑기
+  for (let i = 0; i < moves.length; i++) {
+    
+    for (let j = 0; j < board.length; j++) {
+
+      const doll = board[j][moves[i]];
+
+      if (doll) {
+        const lastDoll = stack[stack.length - 1];
+
+        if (lastDoll === doll) {
+          stack.pop();
+          cnt++;
+        } else {
+          stack.push(doll);
+        }
+
+        board[j][moves[i]] = 0;
+        break;
+      }
+    }
+  }
+
+  return cnt * 2;
+}
+
+console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]));
