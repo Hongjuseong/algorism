8c4ed490941a70d01cfddc09ec5b9931a28dc702
Hongjuseong <hjs21847@naver.com>
2021-05-19 22:46:20 +0900

    섹션 6 쇠막대기

---
+++ section_06/5. 쇠막대기 자르기.js
@@ -0,0 +1,28 @@
+function solution(str) {
+  const stack = [];
+  let result = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    
+    if (str[i] === '(') {
+      stack.push(str[i]);
+    } else {
+      const prev = str[i - 1];
+      stack.pop();
+      // console.log(str[i - 1]);
+
+      if (prev === '(') {
+        result += stack.length;
+        // stack.pop();
+      } else {
+        result++;
+      }
+    }
+  }
+
+  return result;
+
+}
+
+console.log(solution('()(((()())(())()))(())'));
+console.log(solution('(((()(()()))(())()))(()())'));
