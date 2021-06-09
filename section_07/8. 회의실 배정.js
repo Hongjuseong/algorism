bc7a2f83ab4800e011b424ae4e24fa0d5d66101d
Hongjuseong <hjs21847@naver.com>
2021-06-09 19:51:19 +0900

    섹션 7 8번 문제 풀이

---
+++ section_07/8. 회의실 배정.js
@@ -0,0 +1,30 @@
+function solution(arr) {
+  const conf = [...arr];
+  let result = 0;
+  let currEnd = 0;
+
+  conf.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+
+    return a[1] - b[1];
+  });
+
+  for (let i = 0; i < conf.length; i++) {
+    const [confStart, confEnd] = conf[i];
+    // console.log(confStart);
+    // console.log(confEnd);
+
+    if (confStart >= currEnd) {
+      currEnd = confEnd;
+      result++;
+    }
+
+  }
+
+  return result;
+}
+
+// console.log(solution([[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]]));
+console.log(solution([[3, 3], [1, 3], [2, 3]]));
