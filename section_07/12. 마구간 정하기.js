1effa60298d4bc7c616fa2fa407a80bab1f41f8e
Hongjuseong <hjs21847@naver.com>
2021-06-18 22:11:45 +0900

    섹션 7 12번 문제 풀이

---
+++ section_07/12. 마구간 정하기.js
@@ -0,0 +1,41 @@
+function solution(arr, cnt) {
+  const stables = arr.sort((a, b) => a - b);
+
+  let end = stables[stables.length - 1];
+  let start = stables[0];
+  let result = Number.MIN_SAFE_INTEGER;
+  
+  while (start <= end) {
+    const mid = parseInt((start + end) / 2);
+    // console.log(mid);
+
+    if (horseCount(stables, mid) < cnt) {
+      end = mid - 1;
+    } else {
+      // 2가 나왔을 때 더 나은 값을 찾기 위한 로직임
+      start = mid + 1;
+    }
+
+    result = mid;
+  }
+
+  return result;
+}
+
+function horseCount(stables, mid) {
+  let horseCount = 1; // 무조건 1번 좌표 한 마리
+  let lastStable = stables[0];
+
+  for (let i = 1; i < stables.length; i++) {
+    const stable = stables[i];
+
+    if (stable - lastStable > mid) {
+      horseCount++;
+      lastStable = stable;
+    } 
+  }
+
+  return horseCount;
+}
+
+console.log(solution([1, 2, 8, 4, 9], 3));
