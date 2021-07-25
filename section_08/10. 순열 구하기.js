0ceebd448d35980f1a549c33ef777aa18254f14d
Hongjuseong <hjs21847@naver.com>
2021-07-25 22:07:25 +0900

    수정

--- section_08/10. 순열 구하기.js
+++ section_08/10. 순열 구하기.js
@@ -1,33 +1,31 @@
-function solution(cnt, arr) {
-  let answer = [];
-  let check = Array.from({length: cnt}).fill(0);
+// function solution(cnt, arr) {
+//   let answer = [];
+//   let check = Array.from({length: cnt}).fill(0);
  
-  function DFS(num, prev) {
+//   function DFS(num, prev) {
 
-    if (num === cnt) {
-      // console.log(check);
-      answer.push([...check]);
-      return;
-    } else {
+//     if (num === cnt) {
+//       // console.log(check);
+//       answer.push([...check]);
+//       return;
+//     } else {
       
-      for (let i = 0; i < arr.length; i++) {
-        // console.log(`num = ${num}`);
-        // console.log(`i = ${i}`);
-        if (prev !== arr[i]) {
-          check[num] = arr[i];  
-          DFS(num + 1, check[num]);
-        }
-      }
-    }
-  }
+//       for (let i = 0; i < arr.length; i++) {
+//         // console.log(`num = ${num}`);
+//         // console.log(`i = ${i}`);
+//         if (prev !== arr[i]) {
+//           check[num] = arr[i];  
+//           DFS(num + 1, check[num]);
+//         }
+//       }
+//     }
+//   }
 
-  DFS(0, 0);
+//   DFS(0, 0);
 
-  answer.push(answer.length);
-  return answer;
-}
-
-console.log(solution(2, [3, 6, 9]));
+//   answer.push(answer.length);
+//   return answer;
+// }
 
 // function solution(m, arr) {
 // 	let answer = 0;
@@ -50,30 +48,31 @@ console.log(solution(2, [3, 6, 9]));
 // 	return answer;
 // }
 
-// function solution(m, arr) {
-// 	let answer = [];
-// 	let n = arr.length;
-// 	let ch = Array.from({ length: n }, () => 0);
-// 	let tmp = Array.from({ length: m }, () => 0);
+function solution(m, arr) {
+	let answer = [];
+	let n = arr.length;
+	let ch = Array.from({ length: n }, () => 0);
+	let tmp = Array.from({ length: m }, () => 0);
 	
-// 	function DFS(L) {
-// 		if (L === m) {
-// 			answer.push(tmp.slice());
-// 		} else {
-// 			for (let i = 0; i < n; i++) {
-// 				if (ch[i] === 0) {
-// 					ch[i] = 1;
-// 					tmp[L] = arr[i];
-// 					DFS(L+1);
-// 					ch[i] = 0;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	DFS(0);
+	function DFS(L) {
+		if (L === m) {
+			answer.push(tmp.slice());
+		} else {
+			for (let i = 0; i < n; i++) {
+				if (ch[i] === 0) {
+					ch[i] = 1;
+					tmp[L] = arr[i];
+					DFS(L+1);
+					ch[i] = 0;
+				}
+			}
+		}
+	}
+	DFS(0);
 	
-// 	return answer;
-// }
+	return answer;
+}
 
-// console.log(solution(3, [3, 6, 9, 12]));
+console.log(solution(2, [3, 6, 9]));
+console.log(solution(3, [3, 6, 9, 12]));
 
